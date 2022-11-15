7759d2301e9d46fecd9696e1a7ba673fa124410e
ismat27 <adegbiteismail27@gmail.com>
2022-11-15 07:53:40 -0800

    iniate dashboard layout

---
+++ src/components/DashboardLayout.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import Logo from './Logo'
+import { Outlet } from 'react-router-dom'
+import styled from 'styled-components'
+import Hamburger from './Hamburger'
+
+import { useGlobalContext } from './context'
+
+const DashboardLayout = () => {
+    const {isSidebarOpen, openSidebar} = useGlobalContext()
+  return (
+    <Wrapper>
+        <Sidebar />
+        <div className='page-container'>
+            <div className='header'>
+                <Hamburger open={isSidebarOpen} handleClick={openSidebar} />
+                <Logo />
+            </div>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+position: relative;
+.page-container {
+    flex: auto;
+    padding: 10px 10px;
+}
+.header {
+    display: flex;
+    justify-content: space-between;
+}
+@media (min-width: 992px) {
+    display: flex;
+    .header {
+        justify-content: flex-end;
+    }
+    .menu {
+        display: none;
+    }
+    .header img {
+        width: 300px;
+    }
+}
+`
+
+export default DashboardLayout
